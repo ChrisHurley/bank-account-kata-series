dcedf3ffa5177c650d535036d9339c5b7d978003
Chris Hurley <chris.hurley@red-gate.com>
2015-08-05 15:21:04 +0100

    Differentiate between check and cash debit entry

---
+++ BankingKata/CashDebitEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingKata
+{
+    public class CashDebitEntry : DebitEntry
+    {
+        public CashDebitEntry(DateTime now, Money money) :base(now, money)
+        {
+           
+        }
+    }
+}
---
+++ BankingKata/CheckDebitEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BankingKata
+{
+    public class CheckDebitEntry : DebitEntry
+    {
+        public CheckDebitEntry(DateTime now, Money money):base(now, money)
+        {
+           
+        }
+    }
+}
