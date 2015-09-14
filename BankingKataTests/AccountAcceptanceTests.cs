1c1cefdc5efefcdb813da984dadc45999dcfba7e
Chris Hurley <chris.hurley@red-gate.com>
2015-09-14 16:27:33 +0100

    Arranged overdraft impl and tests
    This has a horrible hack :(

--- BankingKataTests/AccountAcceptanceTests.cs
+++ BankingKataTests/AccountAcceptanceTests.cs
@@ -47,5 +47,40 @@ namespace BankingKataTests
             var debitEntry = new ATMDebitEntry(DateTime.Now, new Money(999m));
             Assert.DoesNotThrow(() => accountWithOverdraft.Withdraw(debitEntry));
         }
+
+        [Test]
+        public void WithdrawingMoreThanArrangedOverdraftLimit()
+        {
+            var accountWithOverdraft = new AccountWithOverdraft(new Account(), new ArrangedOverdraft(new Money(-200m), new Money(2.50m)));
+
+            var debitEntry = new ATMDebitEntry(DateTime.Now, new Money(201m));
+            Assert.DoesNotThrow(() => accountWithOverdraft.Withdraw(debitEntry));
+
+            var balance = accountWithOverdraft.CalculateBalance();
+            Assert.That(balance, Is.EqualTo(new Money(-203.5m)));
+        }
+
+        [Test]
+        public void WithdrawingMoreThanArrangedOverdraftLimitCanExceedUnarrangedLimitDueToCharge()
+        {
+            var accountWithArrangedOverdraft = new AccountWithOverdraft(new Account(), new ArrangedOverdraft(new Money(-200m), new Money(2.50m)));
+            var accountWithUnarrangedOverdraft = new AccountWithOverdraft(accountWithArrangedOverdraft, new UnarrangedOverdraft(new Money(-202m)));
+
+            var debitEntry = new ATMDebitEntry(DateTime.Now, new Money(201m));
+            Assert.DoesNotThrow(() => accountWithUnarrangedOverdraft.Withdraw(debitEntry));
+
+            var balance = accountWithUnarrangedOverdraft.CalculateBalance();
+            Assert.That(balance, Is.EqualTo(new Money(-203.5m)));
+        }
+
+        [Test]
+        public void WithdrawingMoreThanUnarrangedOverdraftLimitWhenArrangedOverdraftExistsThrowsException()
+        {
+            var accountWithArrangedOverdraft = new AccountWithOverdraft(new Account(), new ArrangedOverdraft(new Money(-200m), new Money(2.50m)));
+            var accountWithUnarrangedOverdraft = new AccountWithOverdraft(accountWithArrangedOverdraft, new UnarrangedOverdraft(new Money(-202m)));
+
+            var debitEntry = new ATMDebitEntry(DateTime.Now, new Money(203m));
+            Assert.Throws<OverdraftLimitReachedException>(() => accountWithUnarrangedOverdraft.Withdraw(debitEntry));
+        }
     }
 }
